33967775c75aaf759f3df4e4920afa8badd607bb
phong28zk <PhongTN.B20VT288@stu.ptit.edu.vn>
2024-04-26 05:50:35 +0700

    (Chore) deploy Vercel

--- src/app/blogs/layout.tsx
+++ src/app/blogs/layout.tsx
@@ -6,7 +6,7 @@ export const metadata = {
 };
 export default function BlogsLayout({ children }: { children: React.ReactNode }) {
   return (
-    <section className="top-0 w-4/5 h-full z-50 block px-8 pt-4 xl:w-1/2 lg:pl-8 lg:pr-8 lg:w-[calc(50% + 12.5vw)] md:pl-8 md:pr-8 md:w-[80%]">
+    <section className="top-0 w-4/5 h-full z-50 block px-8 pt-4 xl:w-1/2 lg:pl-8 lg:pr-8 lg:w-[calc(50% + 12.5vw)] md:pl-8 md:pr-10 md:w-[80%]">
       {children}
     </section>
   );
