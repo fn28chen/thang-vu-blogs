d7e0def277c1889150036dc50f7c6accbfd90e7d
phong28zk <PhongTN.B20VT288@stu.ptit.edu.vn>
2024-04-26 17:26:52 +0700

    (Style) Fix layout of recent blogs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { ICardProps } from "@/lib/type/card";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { readClient } from "../../sanity/lib/client";
+import { Card } from "@/components/ui/card";
 
 const Home = (props: {}) => {
   const [blogs, setBlogs] = useState<ICardProps[]>([]);
@@ -19,17 +20,20 @@ const Home = (props: {}) => {
       <section
         className="
         flex flex-col gap-4
-        fixed top-0 w-full h-full z-50 px-8 pt-12 pb-12 xl:w-[50%] lg:pl-8 lg:pr-8 lg:w-[calc(50% + 20vw)] md:pl-8 md:pr-8 md:w-[60%] sm:w-[60%]
+        fixed top-0 w-full h-full z-50 px-8 pt-12 pb-12 xl:w-[66%] lg:pl-8 lg:pr-8 lg:w-[calc(50% + 20vw)] md:pl-8 md:pr-8 md:w-[80%] sm:w-[70%]
         min-h-screen p-8 relative
         "
       >
-        <div className="flex flex-row">
+        <div className="flex flex-row gap-4">
           <div className="leading-none">
-            <h2 className="dark:text-zinc-200 text-zinc-900 text-[48px] m-0 font-extrabold">
+            <h2 className="dark:text-zinc-200 text-zinc-900 text-[44px] m-0 font-extrabold md:block hidden">
               Vu Nguyen Duc Thang
             </h2>
+            <h2 className="dark:text-zinc-200 text-zinc-900 text-[44px] m-0 font-extrabold md:hidden">
+              Thang Vu
+            </h2>
             <p className="dark:text-zinc-300 text-zinc-800 mb-4 m-0"></p>
-            <p className="dark:text-zinc-400 text-zinc-700 text-sm m-0">
+            <p className="dark:text-zinc-400 text-zinc-700 text-sm text-justify m-0">
               Learning about web and trying to help out other devs in the
               process. I love open source and writing about tech occasionally.
             </p>
@@ -44,19 +48,19 @@ const Home = (props: {}) => {
             />
           </div>
         </div>
-        <div>
+        <span className="font-bold text-2xl">Recent Blogs</span>
+        <div className="grid grid-rows-3 lg:grid lg:grid-cols-3 gap-4">
           {blogs?.length > 0 ? (
             blogs.map((post) => (
-              <div
+              <Card
                 key={post._id}
-                className="flex flex-row text-justify justify-between px-4 py-4 gap-4"
+                className="flex flex-col text-justify justify-between px-4 py-4 gap-4"
               >
-                <span className="flex flex-col gap-2">
-                  <h2 className="font-bold text-2xl">{post.title}</h2>
-                  <p className="">{post.description}</p>
-                  <p className="">{post._createdAt}</p>
+                <span className="flex flex-col gap-2 justify-between h-full">
+                  <h2 className="font-bold">{post.title}</h2>
+                  <p className="mt-auto text-[12px]">{post._createdAt}</p>
                 </span>
-              </div>
+              </Card>
             ))
           ) : (
             <div className="flex flex-row text-justify justify-between px-4 py-4 gap-4">
