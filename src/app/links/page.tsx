3ad7839847408d643841eb61a51cada1c5614d3d
phong28zk <PhongTN.B20VT288@stu.ptit.edu.vn>
2024-05-05 12:37:38 +0700

    (Style) Update better css - links & now

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -3,19 +3,17 @@ import LinksComponent from "@/components/global/link";
 
 export default function Links() {
   return (
-    <div className="w-full min-h-screen h-full flex flex-col items-center relative">
-      <section className="prose mt-8 flex w-[80%] flex-col justify-between  gap-6 md:mt-0 lg:mt-0">
-        <div className="flex flex-col gap-6">
-          <h1 className="dark:text-zinc-200 text-zinc-900 leading-none mb-3 text-[48px] font-bold justify-center pt-3">
-            Links
-          </h1>
-          <p className="dark:text-zinc-400 text-zinc-800 m-0 leading-tight">
-            All my profile links to find me on the web.
-          </p>
-        </div>
-        <LinksComponent />
-        <Footer />
-      </section>
-    </div>
+    <section className="w-full">
+      <div className="flex flex-col gap-6">
+        <h1 className="dark:text-zinc-200 text-zinc-900 leading-none mb-3 text-[48px] font-bold justify-center pt-3">
+          Links
+        </h1>
+        <p className="dark:text-zinc-400 text-zinc-800 m-0 leading-tight pb-4">
+          All my profile links to find me on the web.
+        </p>
+      </div>
+      <LinksComponent />
+      <Footer />
+    </section>
   );
 }
